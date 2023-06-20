f3755aaefe62cde3d2da6e20f24fc57e71d9d872
shridhar7393 <shreeheg@gmail.com>
2023-06-20 10:50:20 +0530

    Update private key reading

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 require('dotenv').config();
 const AWS = require("aws-sdk");
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
+const pemFilePath = './rsa-private-key.pem';
+const pemFileContent = fs.readFileSync(pemFilePath, 'utf8');
 
 app.get('/getImage', (req, res) => {
   const signedUrlParams = {
@@ -11,7 +14,9 @@ app.get('/getImage', (req, res) => {
     expires: Math.floor(Date.now() / 1000) + 300, // set expire time for 5 mins.
   };
 
-  new AWS.CloudFront.Signer(process.env.CLOUDFRONT_KEY_PAIR_ID, process.env.CLOUDFRONT_PK.replace(/\\n/g, '\n'))
+ 
+
+  new AWS.CloudFront.Signer(process.env.CLOUDFRONT_KEY_PAIR_ID, pemFileContent.toString())
     .getSignedUrl(signedUrlParams, (err, url) => {
       if (err) {
         console.error('Error generating signed URL:', err);
